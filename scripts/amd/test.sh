86b2c6100908a5b22a7468463c0efd124465bffc
Michael Melesse <micmelesse@gmail.com>
2021-04-28 16:29:09 -0500

    disable test

--- scripts/amd/test.sh
+++ scripts/amd/test.sh
@@ -1,2 +1,2 @@
 # pytest python/test
-pytest python/test/test_blocksparse.py::test_matmul[sdd-False-False-16-float16]
+# pytest python/test/test_blocksparse.py::test_matmul[sdd-False-False-16-float16]
