5c998f197633dfa073236af2fb8e68e4220e449e
Michael Melesse <micmelesse@gmail.com>
2021-05-12 17:09:33 -0500

    update test

--- scripts/amd/test.sh
+++ scripts/amd/test.sh
@@ -1,5 +1,8 @@
-# pytest --verbose python/test
+export TRITON_LIBHIP=/opt/rocm/lib/libamdhip64.so
+
+# pytest python/test
 # pytest python/test/test_blocksparse.py 
 
-# export TRITON_LIBHIP=/opt/rocm/lib/libamdhip64.so
-pytest --verbose python/test/test_blocksparse.py::test_matmul[sdd-False-False-16-float16]
+pytest --verbose python/test/test_conv.py
+# pytest --verbose python/test/test_blocksparse.py::test_matmul[sdd-False-False-16-float16]
+# pytest --verbose python/test/test_blocksparse.py::test_attention_fwd_bwd
