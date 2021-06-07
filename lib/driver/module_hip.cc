9d8cc2720ddad553637816eb1c55e1ae20d40a88
Michael Melesse <micmelesse@gmail.com>
2021-06-07 14:06:36 -0500

    add feature str look up

--- lib/driver/module_hip.cc
+++ lib/driver/module_hip.cc
@@ -216,6 +216,58 @@ static std::map<int, int> vptx = {
   {11020, 72}
 };
 
+#define TF_ROCM_VERSION 42000
+std::string MapGCNArchNameTokenToFeatureStr(const std::string& token) {
+  if (token == "sramecc+") {
+    return "+sramecc";
+  } else if (token == "sramecc-") {
+#if TF_ROCM_VERSION < 40100
+    return "";
+#else
+    return "-sramecc";
+#endif
+  } else if (token == "xnack+") {
+    return "+xnack";
+  } else if (token == "xnack-") {
+    return "-xnack";
+  }
+  return "";
+
+}
+
+std::pair<std::string, std::string> GetFeatureStrFromGCNArchName(
+    const std::string& gcn_arch_name) {
+  std::string feature_str;
+  std::string gfx = gcn_arch_name;
+#if TF_ROCM_VERSION < 30900
+  // For ROCm versions older than 3.9, hardcode it to "+code-object-v3"
+  // This is simply to preserve how things were...nohing else
+  feature_str = "+code-object-v3";
+#elif TF_ROCM_VERSION < 40000
+  // For ROCM versions 3.9 and 3.10, hardcode it to empty string
+  feature_str = "";
+#else
+  // For ROCm versions 4.0 and greater, we need to specify the correct
+  // feature str, based on the underlying GPU HW to get max performance.
+  std::vector<std::string> tokens = absl::StrSplit(gcn_arch_name, ':');
+  std::vector<std::string> mapped_tokens;
+  if(tokens.size()>0)
+    gfx=tokens[0];
+  for (auto it = tokens.begin(); it != tokens.end(); it++) {
+    // Skip the first token, that is the gfxNNN str
+    // The rest of the tokens are the feature/targetid strings
+    if (it != tokens.begin()) {
+      std::string token(*it);
+      std::string mapped_token = MapGCNArchNameTokenToFeatureStr(token);
+      mapped_tokens.push_back(mapped_token);
+    }
+  }
+  feature_str = absl::StrJoin(mapped_tokens, ",");
+#endif
+
+  return make_pair(gfx, feature_str);
+}
+
 std::string cu_module::compile_llvm_module(std::unique_ptr<llvm::Module> module, driver::device* device) {
   std::cout << "cu_module::compile_llvm_module" << std::endl;
   // LLVM version in use may not officially support target hardware
@@ -260,8 +312,9 @@ std::string cu_module::compile_llvm_module(std::unique_ptr<llvm::Module> module,
   // std::string features = "code-object-v3";
   // std::string features = "-code-object-v3";
   // std::string features = "+sramecc"; //TODO grep for sram
-  std::string features = ""; //TODO grep for sram
+  // std::string features = ""; //TODO grep for sram
   // std::string features="-ptx60";
+  std::string features = GetFeatureStrFromGCNArchName(proc)[1];
 #endif
   init_llvm();
   // verify and store llvm
@@ -305,10 +358,9 @@ std::string cu_module::compile_llvm_module(std::unique_ptr<llvm::Module> module,
   std::cout << ec << std::endl;
 
   // emit
-  llvm::TargetLibraryInfoWrapperPass* p = new llvm::TargetLibraryInfoWrapperPass(llvm::Triple(module->getTargetTriple()));
-  pass.add(p);
+  // llvm::TargetLibraryInfoWrapperPass* p = new llvm::TargetLibraryInfoWrapperPass(llvm::Triple(module->getTargetTriple()));
+  // pass.add(p);
   machine->addPassesToEmitFile(pass, *isabin_fs, nullptr, llvm::CodeGenFileType::CGFT_ObjectFile);
-  
   return "";
   pass.run(*module);
 
